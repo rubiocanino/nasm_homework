35a37ac0a6682f5fdd7c1a491e83af04d7816148
Saul Rubio <saulrubio98@hotmail.com>
2019-05-06 11:33:50 -0500

    Vowels count running

--- 7.vowels_count/vowels_count.asm
+++ 7.vowels_count/vowels_count.asm
@@ -29,55 +29,40 @@ section .data
     charLine db 0ax
 
 section .bss
-    string  resw 50
-    temp    resb 1
-    length  resb 1
-    i       resb 1
-    countA  resb 1
-    countE  resb 1
-    countI  resb 1
-    countO  resb 1
-    countU  resb 1
+    string  resw 50     ; Variable to reserver the memory of the input 
+    temp    resb 1      ; Variable temporal to copy the value of the string
+    length  resb 1      ; Variable to establish the jump, meaning char by char of the string capturated
+    i       resb 1      ; Counter
+    countA  resb 1      ; Counter for letter A/a
+    countE  resb 1      ; Counter for letter E/e
+    countI  resb 1      ; Counter for letter I/i
+    countO  resb 1      ; Counter for letter O/o
+    countU  resb 1      ; Counter for letter U/u
 
 section .text
-
-global _start
+    global _start
 
 _start:
-    call _printWelcome
-    call _printInputMessage
-    call _captureString
-
-_printWelcome:
-    mov edx, intLenWelcome
-    mov ecx, welcomeMessage
-    mov ebx, STDOUT
-    mov eax, SYS_WRITE
+    mov eax,SYS_WRITE
+    mov ebx,STDOUT
+    mov ecx,welcomeMessage  ; We display the version of the program
+    mov edx,intLenWelcome
     int 0x80
 
-_printInputMessage:
-    mov edx, lenInpuntMessage
-    mov ecx, inputMessage
-    mov ebx, STDOUT
-    mov eax, SYS_WRITE
+    mov eax,SYS_WRITE
+    mov ebx,STDOUT
+    mov ecx,inputMessage    ; We display the input message
+    mov edx,lenInpuntMessage
     int 0x80
 
-_captureString:
-    mov eax, SYS_READ
-    mov ebx, STDIN
-    mov ecx, string
-    mov edx, 2
-    int 0x80
-
-    mov rbx, string
+    mov rbx,string          ; We capture the input
     mov byte[length],0
-    int 0x80
 
-_accepting_string:
+
+_acceptingString:
     push rbx
-
     mov eax,3
-    mov rbx,0
+    mov ebx,0
     mov ecx,temp
     mov edx,1
     int 80h
@@ -91,77 +76,92 @@ _accepting_string:
     inc rbx
 
     cmp byte[temp],10
-    jne _accepting_string
+    jne _acceptingString
 
     dec rbx
     dec byte[length]
     mov byte[rbx],0
 
-    mov byte[countA],0
-    mov byte[countE],0
-    mov byte[countI],0
-    mov byte[countO],0
-    mov byte[countU],0
+
+    mov byte[countA],0  ; We assign the value of 0 the unique byte of the counter A
+    mov byte[countE],0  ; We assign the value of 0 the unique byte of the counter E
+    mov byte[countI],0  ; We assign the value of 0 the unique byte of the counter I
+    mov byte[countO],0  ; We assign the value of 0 the unique byte of the counter Os
+    mov byte[countU],0  ; We assign the value of 0 the unique byte of the counter U
 
     mov byte[i],0
 
 _vowelCheck:
-    mov eax,string
-    movzx ecx,byte[i]
-    add eax,ecx
+    mov eax,string      ; We point to the input variable
+    movzx ecx,byte[i]   ; Now we get where is the counter i
+    add eax,ecx         ; We point the n character of the string adding the value of i
 
-    cmp byte[eax],97
-    je _countA
+    cmp byte[eax],97   ; 97 = a
+    je _countA         ; If the value of the char is 97 countA = +1
 
-    cmp byte[eax],101
-    je _countE
+    cmp byte[eax],65   ; 65 = A
+    je _countA         ; If the value of the char is 65 countA = +1
 
-    cmp byte[eax],105
-    je _countI
+    cmp byte[eax],101   ; 101 = e
+    je _countE          ; If the value of the char is 101 countE = +1
 
-    cmp byte[eax],111
-    je _countO
+    cmp byte[eax],69   ; 69 = E
+    je _countE         ; If the value of the char is 69 countE = +1
 
-    cmp byte[eax],117
-    je _countU
+    cmp byte[eax],105   ; 105 = i
+    je _countI          ; If the value of the char is 105 countI = +1
 
-    continue:
-    inc byte[i]
-    mov al,byte[length]
-    cmp byte[i],al
-    jl _vowelCheck
+    cmp byte[eax],73   ; 73 = I
+    je _countI         ; If the value of the char is 73 countI = +1
 
-    add byte[countA],30h
-    add byte[countE],30h
-    add byte[countI],30h
-    add byte[countO],30h
-    add byte[countU],30h
+    cmp byte[eax],111   ; 111 = o
+    je _countO          ; If the value of the char is 111 countO = +1
 
-    call _print
+    cmp byte[eax],79   ; 79 = O
+    je _countO         ; If the value of the char is 79 countO = +1
 
-    jmp _closeProgram
+    cmp byte[eax],117   ; 117 = u
+    je _countU          ; If the value of the char is 117 countU = +1
 
-_countA:
-    add byte[countA],1
-    jmp continue
+    cmp byte[eax],85    ; 85 = U
+    je _countU          ; If the value of the char is 85 countU = +1
 
-_countE:
-    add byte[countE],1
-    jmp continue
+_continue:
+    inc byte[i]             ; We increment the counter of i, meaning the next char of our string capturated
+    mov al,byte[length]     ; We point to next char in the memory space
+    cmp byte[i],al          ; We compare the the counter with the next char of the string
+    jl _vowelCheck          ; If the string is not null, then go back to check the char
 
-_countI:
-    add byte[countI],1
-    jmp continue
+    add byte[countA],30h    ; We add the value of 0, to get the decimal value to printed in the next subrutine
+    add byte[countE],30h    ; We add the value of 0, to get the decimal value to printed in the next subrutine
+    add byte[countI],30h    ; We add the value of 0, to get the decimal value to printed in the next subrutine
+    add byte[countO],30h    ; We add the value of 0, to get the decimal value to printed in the next subrutine
+    add byte[countU],30h    ; We add the value of 0, to get the decimal value to printed in the next subrutine
 
-_countO:
-    add byte[countO],1
-    jmp continue
+    call _printResult       ; We print the counters
+    call _closeProgram      ; We close the program
 
-_countU:
-    add byte[countU],1
-    jmp continue
+    _countA:
+    add byte[countA],1      ; countA+1
+    jmp _continue
 
-_print:
+    _countE:
+    add byte[countE],1      ; countE+1
+    jmp _continue
+
+    _countI:
+    add byte[countI],1      ; countI+1
+    jmp _continue
+
+    _countO:
+    add byte[countO],1      ; countO+1
+    jmp _continue
+
+    _countU:
+    add byte[countU],1      ; countU+1
+    jmp _continue
+
+_printResult:
     mov eax,SYS_WRITE
     mov ebx,STDOUT
     mov ecx,messageA
@@ -174,7 +174,7 @@ _print:
     mov edx,1
     int 80h
 
-    ;call _printLine
+    call _printLine
 
     mov eax,SYS_WRITE
     mov ebx,STDOUT
@@ -188,7 +188,7 @@ _print:
     mov edx,1
     int 80h
 
-    ;call _printLine
+    call _printLine
 
     mov eax,SYS_WRITE
     mov ebx,STDOUT
@@ -202,7 +202,7 @@ _print:
     mov edx,1
     int 80h
 
-;    call _printLine
+    call _printLine
 
     mov eax,SYS_WRITE
     mov ebx,STDOUT
@@ -216,7 +216,7 @@ _print:
     mov edx,1
     int 80h
 
- ;   call _printLine
+    call _printLine
 
     mov eax,SYS_WRITE
     mov ebx,STDOUT
@@ -230,16 +230,16 @@ _print:
     mov edx,1
     int 80h
 
-  ;  call _printLine
-
+    call _printLine
     ret
 
 _printLine:
-    mov edx, 1
-    mov ecx, charLine
-    mov rbx, STDOUT
-    mov eax, SYS_WRITE
-    int 0x80
+    mov eax,SYS_WRITE
+    mov ebx,STDOUT
+    mov ecx,charLine
+    mov edx,1
+    int 80h
+    ret
 
 _closeProgram:
     mov eax, SYS_EXIT
