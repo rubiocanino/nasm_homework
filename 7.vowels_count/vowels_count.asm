b605855cb5fa14973d16b451c8dccae0cbfccdfb
Saul Rubio <saulrubio98@hotmail.com>
2019-05-06 13:27:41 -0500

    Counting vowels running

--- 7.vowels_count/vowels_count.asm
+++ 7.vowels_count/vowels_count.asm
@@ -55,45 +55,46 @@ _start:
     mov edx,lenInpuntMessage
     int 0x80
 
-    mov rbx,string          ; We capture the input
-    mov byte[length],0
-
+    mov rbx,string          ; We point rbx to the value of the input that we will capture
+    mov byte[length],0      ; And the length of this rbx/input value we start in 0
+                            ; To start counting the size
 
 _acceptingString:
-    push rbx
-    mov eax,3
-    mov ebx,0
-    mov ecx,temp
-    mov edx,1
-    int 80h
+    push rbx                ; We send rbx pointer to the stack
+    mov eax,SYS_READ        ; We capture the string in temp variable
+    mov ebx,STDIN
+    mov ecx,temp   
+    mov edx,1               
+    int 80h                 ; We execute the capture of the input
 
-    pop rbx
+    pop rbx                 ; Now we bring back the pointer rbx
 
-    mov al,byte[temp]
-    mov byte[rbx],al
+    mov al,byte[temp]       ; Now we keep in al the first byte of the temp (input) value
+    mov byte[rbx],al        ; Also we send to the first byte of rbx(string) the value of al, hola  (return,enter)
 
-    inc byte[length]
-    inc rbx
+    inc byte[length]        ; Now we add +1 to the length of our input [0],1
+    inc rbx                 ; And we point to next byte of rbx         [0],1
 
-    cmp byte[temp],10
-    jne _acceptingString
+    cmp byte[temp],10       ; Now we compare the value of the pointer in temp and the value of return [0], 0xa = return
+    jne _acceptingString    ; If the value is not equal to return, then we loop in _acceptingString
+                            ; Because we are determining the length of the inp
 
-    dec rbx
-    dec byte[length]
-    mov byte[rbx],0
+    dec rbx                 ; If the loop breaks, that means that we have the length of the input
+    dec byte[length]        ; So we need to decrese 1 the pointer in rbx and the length
+    mov byte[rbx],0         ; And now we point to the begging of our input
 
 
     mov byte[countA],0  ; We assign the value of 0 the unique byte of the counter A
     mov byte[countE],0  ; We assign the value of 0 the unique byte of the counter E
     mov byte[countI],0  ; We assign the value of 0 the unique byte of the counter I
-    mov byte[countO],0  ; We assign the value of 0 the unique byte of the counter Os
+    mov byte[countO],0  ; We assign the value of 0 the unique byte of the counter O
     mov byte[countU],0  ; We assign the value of 0 the unique byte of the counter U
-
-    mov byte[i],0
+    
+    mov byte[i],0       ; We assign the value of 0 the unique byte of the counter i
 
 _vowelCheck:
     mov eax,string      ; We point to the input variable
-    movzx ecx,byte[i]   ; Now we get where is the counter i
+    movzx ecx,byte[i]   ; Now we get where is the counter i vector[0],[1]
     add eax,ecx         ; We point the n character of the string adding the value of i
 
     cmp byte[eax],97   ; 97 = a
@@ -127,9 +128,9 @@ _vowelCheck:
     je _countU          ; If the value of the char is 85 countU = +1
 
 _continue:
-    inc byte[i]             ; We increment the counter of i, meaning the next char of our string capturated
+    inc byte[i]             ; We increment the counter of i, meaning the next char of our string capturated,[0],[1]
     mov al,byte[length]     ; We point to next char in the memory space
-    cmp byte[i],al          ; We compare the the counter with the next char of the string
+    cmp byte[i],al          ; We compare the the counter with the next char of the string 1 < 10, 11 < 10
     jl _vowelCheck          ; If the string is not null, then go back to check the char
 
     add byte[countA],30h    ; We add the value of 0, to get the decimal value to printed in the next subrutine
